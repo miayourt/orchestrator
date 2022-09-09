65c51b9c6e22feee847ff33472dde07f2d3efc49
EC2 Default User <ec2-user@ip-10-0-0-14.eu-west-3.compute.internal>
2022-09-09 10:01:23 +0000

    version2

--- orchestrator.sh
+++ orchestrator.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-
-echo -n "Infra Name : "
-read INFRA_NAME
-
+INFRA_NAME=$1
 cp -r TEMPLATE DEPLOYED/${INFRA_NAME}
 cd DEPLOYED/${INFRA_NAME}
-
 sed -i "s|<##INFRA_NAME##>|${INFRA_NAME}|g" *
-
-/c/workspace/terraform/bin/terraform init
-/c/workspace/terraform/bin/terraform apply -auto-approve
-
+terraform init
+terraform apply -auto-approve
+IP=$(cat temp_ip)
+NB_SPACE=$(( 16 - $(echo ${IP} | wc -c) ))
+SPACES=""
+I=0
+while [ $I -lt $NB_SPACE ]
+do
+        SPACES=${SPACES}" "
+        I=$(( $I + 1 ))
+done
 cat << EOF
 ############################################################
 ############################################################
 ########                                           #########
 ########                                           #########
-########    Web App Available at : $(cat temp_ip)   #########
+########    Web App Available at : ${IP}${SPACES}#########
 ########                                           #########
 ########                                           #########
 ############################################################
 ############################################################
 EOF
-
 rm temp_ip
 exit
